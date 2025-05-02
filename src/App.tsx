a1b3965057a3311de002162afa3f397c57698575
Pranava Holla <pranavarholla@gmail.com>
2025-05-02 17:53:45 +0530

    Comment out GlobalSparkEffect component in App.tsx for potential future use

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import GlobalSparkEffect from "./components/GlobalSparkEffect";
+// import GlobalSparkEffect from "./components/GlobalSparkEffect";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <GlobalSparkEffect />
+      {/* <GlobalSparkEffect /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
